df6068c968b5ab41646f541eea35b179d6b663be
mbenmaamar <benmaamar.mouloud@gmail.com>
2023-02-23 07:44:45 +0100

    les test vite fait

--- tuto/src/main/java/com/springsecurity/tuto/controller/LoginController.java
+++ tuto/src/main/java/com/springsecurity/tuto/controller/LoginController.java
@@ -1,13 +1,22 @@
 package com.springsecurity.tuto.controller;
 
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.security.RolesAllowed;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springsecurity.tuto.model.MySong;
@@ -21,8 +30,14 @@ public class LoginController {
 	@Autowired
 	MySongsService mySongsService;
 
+	private final OAuth2AuthorizedClientService auth2AuthorizedClientService;
+
+	public LoginController(OAuth2AuthorizedClientService auth2AuthorizedClientService) {
+		this.auth2AuthorizedClientService = auth2AuthorizedClientService;
+	}
+
 	@RolesAllowed("USER")
-	@GetMapping("/*")
+	@GetMapping("/**")
 	public String getUser() {
 		return "Hello i am User";
 	}
@@ -33,7 +48,58 @@ public class LoginController {
 
 		return "Hello i am Admin";
 	}
+ 
+	@RequestMapping("/*")
+	public String getUserInfo(Principal user) {
+		StringBuffer userInfo = new StringBuffer();
 
+		if (user instanceof UsernamePasswordAuthenticationToken) {
+			userInfo.append(getUsernamePasswordLoginInfo(user));
+		} else {
+			if (user instanceof OAuth2AuthenticationToken) {
+				userInfo.append(getOauth2loginInfo(user));
+			}
+		}
+
+		return userInfo.toString();
+	}
+
+	private StringBuffer getUsernamePasswordLoginInfo(Principal user) {
+		StringBuffer userNameInfo = new StringBuffer();
+		UsernamePasswordAuthenticationToken token = ((UsernamePasswordAuthenticationToken) user);
+		if (token.isAuthenticated()) {
+			User u = (User) token.getPrincipal();
+			userNameInfo.append("Hello, " + u.getUsername());
+		} else {
+			userNameInfo.append("Not Found");
+		}
+		return userNameInfo;
+	}
+
+	private StringBuffer getOauth2loginInfo(Principal user) {
+		StringBuffer protectedInfo = new StringBuffer();
+		OAuth2AuthenticationToken authenticationToken = (OAuth2AuthenticationToken) user;
+		OAuth2AuthorizedClient authorizedClient = this.auth2AuthorizedClientService.loadAuthorizedClient(
+				authenticationToken.getAuthorizedClientRegistrationId(), authenticationToken.getName());
+		if (authenticationToken.isAuthenticated()) {
+			Map<String, Object> userAttributes = ((DefaultOAuth2User) authenticationToken.getPrincipal())
+					.getAttributes();
+			String userToken = authorizedClient.getAccessToken().getTokenValue();
+			protectedInfo.append("my name is : " + userAttributes.get("name") + "<br><br>");
+			protectedInfo.append("my email is : " + userAttributes.get("email") + "<br><br>");
+			protectedInfo.append("my token is : " + userToken + "<br><br>");
+
+		} else {
+			protectedInfo.append("Not Found");
+		}
+
+		return protectedInfo;
+	}
+
+	
+	
+	
+	
 	@GetMapping("/binayInt/{value}")
 	public String BinarySearchInt(@PathVariable(value = "value") int valueToSearch) {
 		// binar
@@ -96,11 +162,10 @@ public class LoginController {
 	}
 
 	@GetMapping("/sortSongs/{sort}")
-	public List<MySong>getMyFavSongsRank(@PathVariable(value="sort") String sortType){
-		 
-			List<MySong> sortedSongs = mySongsService.sortedSongs(sortType);
-		 
-		 
+	public List<MySong> getMyFavSongsRank(@PathVariable(value = "sort") String sortType) {
+
+		List<MySong> sortedSongs = mySongsService.sortedSongs(sortType);
+
 		return sortedSongs;
 	}
 
--- tuto/src/test/java/com/springsecurity/tuto/TutoApplicationTests.java
+++ tuto/src/test/java/com/springsecurity/tuto/TutoApplicationTests.java
@@ -1,13 +1,21 @@
 package com.springsecurity.tuto;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.springsecurity.tuto.controller.LoginController;
+
 
 @SpringBootTest
 class TutoApplicationTests {
-
+	@Autowired
+	LoginController controller;
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
